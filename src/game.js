039a4924106fa384f570498cb757e1d754c5a4d3
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-02-28 15:30:52 -0300

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,85 +1,150 @@
-// Escena de precarga (BootScene)
+// Variables globales para tamaños relativos
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+/* ----------------- BootScene: precarga de assets ----------------- */
 class BootScene extends Phaser.Scene {
     constructor() {
         super({ key: 'BootScene' });
     }
     preload() {
-      // Carga de imágenes
-      this.load.image('menuBg', 'assets/images/menu_background.png');
-      this.load.image('logo', 'assets/images/logo.png');
-      this.load.image('trackBg', 'assets/images/track_background.png');
-      this.load.image('playerKart', 'assets/images/milei_kart.png');
-      this.load.image('opponentKart', 'assets/images/opponent_kart.png');
-      this.load.image('powerDesinfo', 'assets/images/power_up_desinformation.png');
-      this.load.image('powerRetuits', 'assets/images/power_up_retuits.png');
-      this.load.image('powerShield', 'assets/images/power_up_shield.png');
-      this.load.image('powerHostigamiento', 'assets/images/power_up_hostigamiento.png');
-      
-      // Carga de sonidos
-      this.load.audio('bgMusic', 'assets/sounds/background_music.mp3');
-      this.load.audio('itemPickup', 'assets/sounds/item_pickup.wav');
-      this.load.audio('attackSound', 'assets/sounds/attack_sound.wav');
-      this.load.audio('collisionSound', 'assets/sounds/collision_sound.wav');
-      this.load.audio('menuSelect', 'assets/sounds/menu_select.wav');
+        // Carga de imágenes
+        this.load.image('menuBg', 'assets/images/menu_background.png');
+        this.load.image('logo', 'assets/images/logo.png');
+        this.load.image('trackBg', 'assets/images/track_background.png');
+        this.load.image('playerKart', 'assets/images/milei_kart.png');
+        this.load.image('opponentKart', 'assets/images/opponent_kart.png');
+        this.load.image('powerDesinfo', 'assets/images/power_up_desinformation.png');
+        this.load.image('powerRetuits', 'assets/images/power_up_retuits.png');
+        this.load.image('powerShield', 'assets/images/power_up_shield.png');
+        this.load.image('powerHostigamiento', 'assets/images/power_up_hostigamiento.png');
+        // Carga de sonidos
+        this.load.audio('bgMusic', 'assets/sounds/background_music.mp3');
+        this.load.audio('itemPickup', 'assets/sounds/item_pickup.wav');
+        this.load.audio('attackSound', 'assets/sounds/attack_sound.wav');
+        this.load.audio('collisionSound', 'assets/sounds/collision_sound.wav');
+        this.load.audio('menuSelect', 'assets/sounds/menu_select.wav');
     }
     create() {
-        // Una vez cargados los assets, se inicia el menú
         this.scene.start('MenuScene');
     }
 }
 
-// Escena del menú principal (MenuScene)
+/* ----------------- MenuScene: pantalla de inicio ----------------- */
 class MenuScene extends Phaser.Scene {
     constructor() {
         super({ key: 'MenuScene' });
     }
     create() {
-        // Fondo del menú
-        this.add.image(400, 300, 'menuBg');
+        // Fondo del menú (ajustado al tamaño del juego)
+        const bg = this.add.image(0, 0, 'menuBg').setOrigin(0).setDisplaySize(GAME_WIDTH, GAME_HEIGHT);
+        
+        // Contenedor del menú
+        const menuContainer = this.add.container(GAME_WIDTH / 2, GAME_HEIGHT / 2);
+        
         // Logotipo del juego
-        this.add.image(400, 150, 'logo');
-        // Botón de "Jugar"
-        const startText = this.add.text(400, 400, 'Jugar', { fontSize: '48px', fill: '#fff' }).setOrigin(0.5);
-        startText.setInteractive();
-        startText.on('pointerdown', () => {
+        const logo = this.add.image(0, -150, 'logo').setScale(0.5);
+        
+        // Botón "Jugar" (texto interactivo)
+        const playText = this.add.text(0, 0, 'Jugar', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        playText.setInteractive({ useHandCursor: true });
+        playText.on('pointerdown', () => {
             this.sound.play('menuSelect');
-            this.scene.start('GameScene');
+            // Ir directamente al juego o al tutorial (aquí vamos al tutorial)
+            this.scene.start('TutorialScene');
         });
+        
+        // Botón "Tutorial"
+        const tutorialText = this.add.text(0, 60, 'Tutorial', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        tutorialText.setInteractive({ useHandCursor: true });
+        tutorialText.on('pointerdown', () => {
+            this.sound.play('menuSelect');
+            this.scene.start('TutorialScene');
+        });
+        
+        // Agregamos todo al contenedor
+        menuContainer.add([logo, playText, tutorialText]);
     }
 }
 
-// Escena principal del juego (GameScene)
+/* ----------------- TutorialScene: instrucciones de juego ----------------- */
+class TutorialScene extends Phaser.Scene {
+    constructor() {
+        super({ key: 'TutorialScene' });
+    }
+    create() {
+        // Fondo para el tutorial (reutilizamos el fondo del menú)
+        const bg = this.add.image(0, 0, 'menuBg').setOrigin(0).setDisplaySize(GAME_WIDTH, GAME_HEIGHT);
+        
+        // Contenedor de tutorial
+        const tutorialContainer = this.add.container(GAME_WIDTH / 2, GAME_HEIGHT / 2);
+        
+        // Título del tutorial
+        const title = this.add.text(0, -200, 'Tutorial', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        
+        // Instrucciones (pueden ser más detalladas)
+        const instructions = 
+            "Controles:\n" +
+            "→ ←: Girar\n" +
+            "↑: Acelerar\n\n" +
+            "Power-ups:\n" +
+            "- Gasolina de Desinformación: Acelera y deja confusión\n" +
+            "- Bombas de Retuits: Explosión que detiene rivales\n" +
+            "- Escudo de Privilegio: Protección temporal\n" +
+            "- Chispas de Hostigamiento: Ataque directo\n\n" +
+            "Recoge power-ups para sumar puntos y evita a tus oponentes.\n\n" +
+            "¡Presiona 'Empezar Carrera' para comenzar!";
+        
+        const instrText = this.add.text(0, -80, instructions, { fontSize: '16px', fill: '#fff', align: 'center', wordWrap: { width: 600 } }).setOrigin(0.5);
+        
+        // Botón para comenzar la carrera
+        const startBtn = this.add.text(0, 180, 'Empezar Carrera', { fontSize: '28px', fill: '#0f0' }).setOrigin(0.5);
+        startBtn.setInteractive({ useHandCursor: true });
+        startBtn.on('pointerdown', () => {
+            this.sound.play('menuSelect');
+            this.scene.start('GameScene');
+        });
+        
+        tutorialContainer.add([title, instrText, startBtn]);
+    }
+}
+
+/* ----------------- GameScene: mecánica de carrera ----------------- */
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
     }
     create() {
-        // Inicia la música de fondo
-        this.bgMusic = this.sound.add('bgMusic', { volume: 0.5, loop: true });
+        // Inicia música de fondo
+        this.bgMusic = this.sound.add('bgMusic', { volume: 0.4, loop: true });
         this.bgMusic.play();
 
-        // Fondo de la pista: se usa un tile sprite para dar efecto de desplazamiento
-        this.track = this.add.tileSprite(400, 300, 800, 600, 'trackBg');
+        // Fondo de la pista usando TileSprite (para efecto de desplazamiento)
+        this.track = this.add.tileSprite(0, 0, GAME_WIDTH, GAME_HEIGHT, 'trackBg').setOrigin(0);
 
-        // Crea el kart del jugador
-        this.player = this.physics.add.sprite(400, 500, 'playerKart');
+        // Contenedor para el jugador (para facilitar ajustes de escala y posición)
+        this.playerContainer = this.add.container(GAME_WIDTH / 2, GAME_HEIGHT - 80);
+        this.player = this.physics.add.sprite(0, 0, 'playerKart');
+        this.player.setScale(0.5);
         this.player.setCollideWorldBounds(true);
-        this.player.setDamping(true);
-        this.player.setDrag(0.99);
-        this.player.setMaxVelocity(200);
+        this.playerContainer.add(this.player);
 
-        // Crea un oponente simple que se mueve horizontalmente
-        this.opponent = this.physics.add.sprite(400, 300, 'opponentKart');
+        // Oponente simple
+        this.opponent = this.physics.add.sprite(GAME_WIDTH / 2, GAME_HEIGHT / 2, 'opponentKart');
+        this.opponent.setScale(0.5);
         this.opponent.setCollideWorldBounds(true);
         this.opponent.setVelocityX(100);
 
-        // Se configuran los controles con las teclas de flechas
-        this.cursors = this.input.keyboard.createCursorKeys();
+        // Contenedor del HUD
+        this.hudContainer = this.add.container(10, 10);
+        this.score = 0;
+        this.scoreText = this.add.text(0, 0, 'Puntos: 0', { fontSize: '24px', fill: '#fff' });
+        this.hudContainer.add(this.scoreText);
 
-        // Grupo para los power-ups
+        // Grupo para power-ups
         this.powerUps = this.physics.add.group();
 
-        // Evento para generar power-ups cada 3 segundos
+        // Evento de generación de power-ups cada 3 segundos
         this.time.addEvent({
             delay: 3000,
             callback: this.spawnPowerUp,
@@ -87,17 +152,14 @@ class GameScene extends Phaser.Scene {
             loop: true
         });
 
-        // Colisión entre el jugador y los power-ups
-        this.physics.add.overlap(this.player, this.powerUps, this.collectPowerUp, null, this);
+        // Controles
+        this.cursors = this.input.keyboard.createCursorKeys();
 
-        // Colisión entre el jugador y el oponente
+        // Colisiones: jugador con power-ups y con oponente
+        this.physics.add.overlap(this.player, this.powerUps, this.collectPowerUp, null, this);
         this.physics.add.collider(this.player, this.opponent, this.handleCollision, null, this);
 
-        // Variable de puntuación y su visualización
-        this.score = 0;
-        this.scoreText = this.add.text(16, 16, 'Puntos: 0', { fontSize: '24px', fill: '#fff' });
-
-        // Simulación de duración de la carrera: 60 segundos
+        // Carrera de 60 segundos
         this.time.delayedCall(60000, () => {
             this.bgMusic.stop();
             this.scene.start('EndScene', { score: this.score });
@@ -110,32 +172,29 @@ class GameScene extends Phaser.Scene {
 
         // Movimiento del jugador
         if (this.cursors.left.isDown) {
-            this.player.setAngularVelocity(-150);
+            this.player.angle -= 2;
         } else if (this.cursors.right.isDown) {
-            this.player.setAngularVelocity(150);
-        } else {
-            this.player.setAngularVelocity(0);
+            this.player.angle += 2;
         }
         if (this.cursors.up.isDown) {
-            this.physics.velocityFromRotation(this.player.rotation, 200, this.player.body.acceleration);
+            this.physics.velocityFromRotation(Phaser.Math.DegToRad(this.player.angle - 90), 200, this.player.body.acceleration);
         } else {
             this.player.setAcceleration(0);
         }
 
-        // Movimiento simple del oponente: rebota en los límites horizontales
-        if (this.opponent.body.blocked.right || this.opponent.x >= 750) {
+        // Movimiento simple del oponente: rebota horizontalmente
+        if (this.opponent.x >= GAME_WIDTH - 50) {
             this.opponent.setVelocityX(-100);
-        } else if (this.opponent.body.blocked.left || this.opponent.x <= 50) {
+        } else if (this.opponent.x <= 50) {
             this.opponent.setVelocityX(100);
         }
     }
 
     spawnPowerUp() {
-        // Selecciona aleatoriamente un tipo de power-up
         const types = ['powerDesinfo', 'powerRetuits', 'powerShield', 'powerHostigamiento'];
         const type = Phaser.Utils.Array.GetRandom(types);
-        const x = Phaser.Math.Between(50, 750);
-        const powerUp = this.physics.add.sprite(x, 0, type);
+        const x = Phaser.Math.Between(50, GAME_WIDTH - 50);
+        const powerUp = this.physics.add.sprite(x, 0, type).setScale(0.5);
         powerUp.setVelocityY(Phaser.Math.Between(50, 150));
         powerUp.type = type;
         this.powerUps.add(powerUp);
@@ -143,7 +202,6 @@ class GameScene extends Phaser.Scene {
 
     collectPowerUp(player, powerUp) {
         this.sound.play('itemPickup');
-        // Suma puntos según el tipo de power-up
         let points = 10;
         if (powerUp.type === 'powerDesinfo') points = 15;
         else if (powerUp.type === 'powerRetuits') points = 20;
@@ -156,13 +214,12 @@ class GameScene extends Phaser.Scene {
 
     handleCollision(player, opponent) {
         this.sound.play('collisionSound');
-        // Resta puntos al chocar con el oponente
         this.score = Math.max(0, this.score - 10);
         this.scoreText.setText('Puntos: ' + this.score);
     }
 }
 
-// Escena final (EndScene)
+/* ----------------- EndScene: pantalla final ----------------- */
 class EndScene extends Phaser.Scene {
     constructor() {
         super({ key: 'EndScene' });
@@ -171,21 +228,27 @@ class EndScene extends Phaser.Scene {
         this.finalScore = data.score;
     }
     create() {
-        this.add.text(400, 250, '¡Carrera terminada!', { fontSize: '48px', fill: '#fff' }).setOrigin(0.5);
-        this.add.text(400, 320, 'Puntuación: ' + this.finalScore, { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-        const restartText = this.add.text(400, 400, 'Volver al Menú', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-        restartText.setInteractive();
-        restartText.on('pointerdown', () => {
+        // Fondo de la pantalla final
+        const bg = this.add.image(0, 0, 'menuBg').setOrigin(0).setDisplaySize(GAME_WIDTH, GAME_HEIGHT);
+        
+        // Contenedor para elementos finales
+        const endContainer = this.add.container(GAME_WIDTH / 2, GAME_HEIGHT / 2);
+        const endText = this.add.text(0, -50, '¡Carrera Terminada!', { fontSize: '40px', fill: '#fff' }).setOrigin(0.5);
+        const scoreText = this.add.text(0, 0, 'Puntuación: ' + this.finalScore, { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
+        const menuBtn = this.add.text(0, 80, 'Volver al Menú', { fontSize: '28px', fill: '#0f0' }).setOrigin(0.5);
+        menuBtn.setInteractive({ useHandCursor: true });
+        menuBtn.on('pointerdown', () => {
             this.scene.start('MenuScene');
         });
+        endContainer.add([endText, scoreText, menuBtn]);
     }
 }
 
-// Configuración del juego
+/* ----------------- Configuración del juego ----------------- */
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: GAME_WIDTH,
+    height: GAME_HEIGHT,
     parent: 'game-container',
     physics: {
         default: 'arcade',
@@ -193,7 +256,11 @@ const config = {
             debug: false
         }
     },
-    scene: [BootScene, MenuScene, GameScene, EndScene]
+    scene: [BootScene, MenuScene, TutorialScene, GameScene, EndScene],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    }
 };
 
 const game = new Phaser.Game(config);
