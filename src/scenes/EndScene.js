85f8497e8308008c052d303080ce11db0ebd801a
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-03-01 12:36:20 -0300

    Actualizar EndScene.js

--- src/scenes/EndScene.js
+++ src/scenes/EndScene.js
@@ -1,35 +1,55 @@
-class EndScene extends Phaser.Scene {
-  constructor() {
-    super({ key: 'EndScene' });
-  }
+export default class EndScene extends Phaser.Scene {
+    constructor() {
+        super('EndScene');
+    }
 
-  init(data) {
-    this.finalScore = data.score;
-  }
+    init(data) {
+        this.winner = data.winner;
+    }
 
-  create() {
-    // Fondo
-    this.add.image(0, 0, 'menuBg').setOrigin(0).setDisplaySize(GAME_WIDTH, GAME_HEIGHT);
+    create() {
+        // Fondo oscuro semi-transparente
+        this.add.rectangle(this.cameras.main.centerX, this.cameras.main.centerY, this.cameras.main.width, this.cameras.main.height, 0x000000, 0.7);
 
-    // Contenedor final
-    const endContainer = this.add.container(GAME_WIDTH / 2, GAME_HEIGHT / 2);
+        // Contenedor de la pantalla final
+        const endContainer = this.add.container(this.cameras.main.centerX, this.cameras.main.centerY);
+        const containerBg = this.add.rectangle(0, 0, 600, 300, 0x111111, 0.8);
+        let endMessage = "";
+        if (this.winner === "player") {
+            endMessage = "¡Felicidades! Has destruido el kart opositor.";
+        } else {
+            endMessage = "¡Tu kart ha sido destruido! La oposición triunfa.";
+        }
+        const endText = this.add.text(0, -80, "Fin de la Carrera", {
+            fontSize: '36px',
+            fill: '#FFD700',
+            fontStyle: 'bold'
+        }).setOrigin(0.5);
+        const resultText = this.add.text(0, -20, endMessage, {
+            fontSize: '28px',
+            fill: '#fff',
+            align: 'center',
+            wordWrap: { width: 550 }
+        }).setOrigin(0.5);
+        const restartButtonBg = this.add.rectangle(0, 80, 200, 60, 0xFF2222).setStrokeStyle(2, 0xffffff);
+        const restartButtonText = this.add.text(0, 80, "REINTENTAR", {
+            fontSize: '26px',
+            fill: '#fff'
+        }).setOrigin(0.5);
 
-    const endText = this.add.text(0, -50, '¡Carrera Terminada!', 
-      { fontSize: '40px', fill: '#fff' }
-    ).setOrigin(0.5);
+        endContainer.add([containerBg, endText, resultText, restartButtonBg, restartButtonText]);
 
-    const scoreText = this.add.text(0, 0, 'Puntuación: ' + this.finalScore, 
-      { fontSize: '32px', fill: '#fff' }
-    ).setOrigin(0.5);
-
-    const menuBtn = this.add.text(0, 80, 'Volver al Menú', 
-      { fontSize: '28px', fill: '#0f0' }
-    ).setOrigin(0.5);
-    menuBtn.setInteractive({ useHandCursor: true });
-    menuBtn.on('pointerdown', () => {
-      this.scene.start('MenuScene');
-    });
-
-    endContainer.add([endText, scoreText, menuBtn]);
-  }
+        // Interactividad del botón para reiniciar la partida
+        restartButtonBg.setInteractive({ useHandCursor: true });
+        restartButtonBg.on('pointerdown', () => {
+            this.sound.play('menuSelect');
+            this.scene.start('MenuScene');
+        });
+        restartButtonBg.on('pointerover', () => {
+            restartButtonBg.setFillStyle(0xFF5555);
+        });
+        restartButtonBg.on('pointerout', () => {
+            restartButtonBg.setFillStyle(0xFF2222);
+        });
+    }
 }
