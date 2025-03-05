435d72aefb48e7c22be06cbe991b38beef1c15f5
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-03-05 11:29:31 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ const config = {
             debug: false
         }
     },
-    scene: [BootScene, PreloadScene, MenuScene, CircuitScene,GameScene, EndScene],
+    scene: [BootScene, PreloadScene, MenuScene, CircuitScene,GameScene, UIScene, EndScene],
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
