368b46ce9ad828bb75ab4ccae134d69796379a6d
ramirosilvera <67182245+ramirosilvera@users.noreply.github.com>
2025-03-05 11:37:58 -0300

    Update UIScene.js

--- src/scenes/UIScene.js
+++ src/scenes/UIScene.js
@@ -7,92 +7,42 @@ export default class UIScene extends Phaser.Scene {
     this.uiContainer = this.add.container(0, 0);
 
     // Barra de salud del jugador (esquina superior izquierda)
-    const playerHealthBg = this.add.rectangle(50, 20, 300, 50, 0x000000, 0.7).setOrigin(0);
-    this.playerHealthBar = this.add.rectangle(50, 20, 300, 50, 0x00ff00).setOrigin(0);
-    const playerHealthText = this.add.text(60, 30, "Jugador: 100", {
-      fontSize: "28px",
-      fill: "#ffffff",
-      fontStyle: "bold",
+    const playerHealthBg = this.add.rectangle(20, 20, 300, 40, 0x000000, 0.8).setOrigin(0);
+    this.playerHealthBar = this.add.rectangle(20, 20, 300, 40, 0x00ff00).setOrigin(0);
+    const playerHealthText = this.add.text(30, 28, "Jugador: 100", {
+      fontSize: "20px",
+      fill: "#fff",
+      fontStyle: "bold"
     });
 
     // Barra de salud del oponente (esquina superior derecha)
-    const opponentHealthBg = this.add.rectangle(
-      this.cameras.main.width - 350,
-      20,
-      300,
-      50,
-      0x000000,
-      0.7
-    ).setOrigin(0);
-    this.opponentHealthBar = this.add.rectangle(
-      this.cameras.main.width - 350,
-      20,
-      300,
-      50,
-      0xff0000
-    ).setOrigin(0);
-    const opponentHealthText = this.add.text(
-      this.cameras.main.width - 340,
-      30,
-      "Oponente: 100",
-      {
-        fontSize: "28px",
-        fill: "#ffffff",
-        fontStyle: "bold",
-      }
-    );
+    const opponentHealthBg = this.add.rectangle(this.cameras.main.width - 320, 20, 300, 40, 0x000000, 0.8).setOrigin(0);
+    this.opponentHealthBar = this.add.rectangle(this.cameras.main.width - 320, 20, 300, 40, 0xff0000).setOrigin(0);
+    const opponentHealthText = this.add.text(this.cameras.main.width - 310, 28, "Oponente: 100", {
+      fontSize: "20px",
+      fill: "#fff",
+      fontStyle: "bold"
+    });
 
     // Cronómetro (central superior)
-    this.timerText = this.add.text(
-      this.cameras.main.width / 2,
-      20,
-      "Tiempo: 0.0s",
-      {
-        fontSize: "28px",
-        fill: "#ffffff",
-        fontStyle: "bold",
-        stroke: "#000",
-        strokeThickness: 4,
-      }
-    )
-      .setOrigin(0.5)
-      .setScrollFactor(0);
+    this.timerText = this.add.text(this.cameras.main.width / 2, 20, "Tiempo: 0.0s", {
+      fontSize: "24px",
+      fill: "#ffcc00",
+      fontStyle: "bold",
+      stroke: "#000",
+      strokeThickness: 3
+    }).setOrigin(0.5).setScrollFactor(0);
 
     // Indicador de progreso (en la parte inferior)
-    const progressBarBg = this.add
-      .rectangle(
-        this.cameras.main.width / 2,
-        this.cameras.main.height - 50,
-        400,
-        30,
-        0x000000,
-        0.7
-      )
-      .setScrollFactor(0);
-    this.progressBar = this.add
-      .rectangle(
-        this.cameras.main.width / 2 - 200,
-        this.cameras.main.height - 50,
-        0,
-        30,
-        0x00ff00
-      )
-      .setOrigin(0, 0.5)
-      .setScrollFactor(0);
-    this.progressText = this.add.text(
-      this.cameras.main.width / 2,
-      this.cameras.main.height - 50,
-      "Progreso: 0%",
-      {
-        fontSize: "20px",
-        fill: "#ffffff",
-        fontStyle: "bold",
-        stroke: "#000",
-        strokeThickness: 3,
-      }
-    )
-      .setOrigin(0.5)
-      .setScrollFactor(0);
+    const progressBarBg = this.add.rectangle(this.cameras.main.width / 2, this.cameras.main.height - 40, 400, 30, 0x000000, 0.8).setScrollFactor(0);
+    this.progressBar = this.add.rectangle(this.cameras.main.width / 2 - 200, this.cameras.main.height - 40, 0, 30, 0x00ff00).setOrigin(0, 0.5).setScrollFactor(0);
+    this.progressText = this.add.text(this.cameras.main.width / 2, this.cameras.main.height - 40, "Progreso: 0%", {
+      fontSize: "18px",
+      fill: "#fff",
+      fontStyle: "bold",
+      stroke: "#000",
+      strokeThickness: 2
+    }).setOrigin(0.5).setScrollFactor(0);
 
     this.uiContainer.add([
       playerHealthBg,
@@ -104,7 +54,7 @@ export default class UIScene extends Phaser.Scene {
       this.timerText,
       progressBarBg,
       this.progressBar,
-      this.progressText,
+      this.progressText
     ]);
 
     // Actualización de la salud
@@ -125,7 +75,6 @@ export default class UIScene extends Phaser.Scene {
 
     // Actualización del indicador de progreso
     this.registry.events.on("updateProgress", (progress) => {
-      // La barra se extiende de 0 a 400 píxeles
       this.progressBar.width = (progress / 100) * 400;
       this.progressText.setText("Progreso: " + progress.toFixed(0) + "%");
     });
